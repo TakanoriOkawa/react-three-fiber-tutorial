9f50309680ee9b9cd08a611c7cec254e5bd01865
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-13 01:36:01 +0900

    Boxコンポーネントを使って使い回し

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import "./App.css"
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+// propsでpositionを受け取る
+function Box(props){
+  const ref = useRef();
+  // fiber用意している関数
+  useFrame(() => (ref.current.rotation.x += 0.01));
+  return(
+    <mesh {...props} ref={ref}>
+      <boxGeometry args={[2,2,2]} />
+      <meshStandardMaterial color={"hotpink"} />
+    </mesh>
+  )
+}
 
 function App() {
   return (
     <div id="canvas-container">
       <Canvas>
-        <mesh position={[-2,0,0]}>
-          <boxGeometry args={[2,2,2]} />
-          <ambientLight intensity={0.5} />
-          <pointLight position={[-10,10,10]} />
-        </mesh>
+        <Box position={[-1.6,0,0]}></Box>
+        <Box position={[1.6,0,0]}></Box>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[-10,10,10]} color={''} />
       </Canvas>
     </div>
   );
