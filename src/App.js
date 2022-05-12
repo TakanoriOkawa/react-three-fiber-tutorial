0103071d24d432b1ea206afcd969d528e8846458
TakanoriOkawa <okawatakanori9916@gmail.com>
2022-05-13 00:57:03 +0900

    mesh boxGeometry などのコンポーネントを表示

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import { Canvas } from "@react-three/fiber";
 function App() {
   return (
     <div id="canvas-container">
-      <Canvas></Canvas>
+      <Canvas>
+        <mesh position={[-2,0,0]}>
+          <boxGeometry args={[2,2,2]} />
+          <ambientLight intensity={0.5} />
+          <pointLight position={[-10,10,10]} />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
